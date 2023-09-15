6debb38cec43acb0a21f1d2cf9d464db5c9dd941
phToan <phamhaitoan10@gmail.com>
2023-09-14 23:06:17 -0700

    new success

--- connect_db.js
+++ connect_db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize( process.env.DBNAME, 'root', process.env.PASSWORD, {
-    host: process.env.HOST,
+const sequelize = new Sequelize( 'b9gvibm1euk2ilsnstt9', 'uelx4ek7hl8p2cgb', 'Posx7EpouyeHaFd0MGAz', {
+    host: 'b9gvibm1euk2ilsnstt9-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     logging: false
 });
@@ -10,7 +10,7 @@ const connectionDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-        console.log(process.env.DB_USERNAME)
+        // console.log(process.env.DB_USERNAME)
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
