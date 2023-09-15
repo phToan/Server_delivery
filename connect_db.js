1bc1b74a6144c9f919238f30bc8aa56f570e30d0
phToan <phamhaitoan10@gmail.com>
2023-09-14 20:44:18 -0700

    ok

--- connect_db.js
+++ connect_db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DBNAME, process.env.USERNAME, process.env.PASSWORD, {
+const sequelize = new Sequelize( process.env.DBNAME, process.env.DB_USERNAME, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: 'mysql',
     logging: false
@@ -10,6 +10,7 @@ const connectionDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
+        console.log(process.env.DB_USERNAME)
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
